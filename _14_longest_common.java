5ebdc1914346d80329405c0676822d5bb8548c6d
Sachin _Tiwari <167885715+sachin11p12@users.noreply.github.com>
2024-10-17 21:34:20 +0530

    Update _14_longest_common.java

--- _14_longest_common.java
+++ _14_longest_common.java
@@ -8,28 +8,7 @@ public class _14_longest_common {
         System.out.println(compare(arr));
     }
     public static String compare(String[] arr) {
-//        String commonPrefix ="";
-//        if (arr.length==0){
-//            System.out.println("Empty String");
-//        } else if (arr.length==1) {
-//            commonPrefix=arr[0];
-//            System.out.println("commonPrefix ="+ commonPrefix);
-//        }
-//        else {
-//            String prefix = arr[0];
-//            for (int i=0;i< arr.length;i++){
-//                for (int j=0; j<prefix.length();j++){
-//                if (arr[i].contains(prefix)){
-//                    commonPrefix = prefix;
-//                    break;
-//                }
-//                else {
-//                    prefix = prefix.substring(0,prefix.length()-1);
-//                }
-//                }
-//                System.out.println(commonPrefix);
-//            }
-//        }
+// Start from here 
         int n = arr.length;
         Arrays.sort(arr);
         int i =0;
